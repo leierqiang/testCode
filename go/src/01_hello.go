44ef5aaae378cd3ac72960218ee3c8be6aba0f9d
hellangels333 <811562287@qq.com>
2019-11-02 17:56:19 +0800

    0

--- go/src/01_hello.go
+++ go/src/01_hello.go
@@ -1,7 +1,25 @@
+// 选择语句 switch
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	num := 2
+	switch num { // switch后面写的是变量本身
+	case 1:
+		fmt.Printf("按下的是1楼\n")
+		//break //go语言默认带跳出，要是不用跳出可以带fallthrough
+	case 2:
+		fmt.Printf("按下的是2楼\n")
+		fallthrough
+	case 3:
+		fmt.Printf("按下的是3楼\n")
+	}
+	score := 85
+	switch {
+	case score > 90:
+		fmt.Println("当前大于90")
+	default:
+		fmt.Println("当没有符合的case时默认输出这句话")
+	}
 }
