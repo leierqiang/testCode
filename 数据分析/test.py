c7d7669ae53d0b476461281dfb4ab8574927cee5
hellangels333 <811562287@qq.com>
2019-12-15 09:16:50 +0800

    .

--- 数据分析/test.py
+++ 数据分析/test.py
@@ -1,5 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 14 20:07:23 2019
+
+@author: Leq
+"""
 from pandas import Series
 #
 s = Series(['a', True, 1], index=['first', 'secont', 'third'])
 
-print(s)
+print(s[1])
