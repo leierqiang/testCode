56effab0ecce07796dc76ce64bbe959901ec7694
hellangels333 <811562287@qq.com>
2019-12-21 09:16:10 +0800

    leq

--- 数据分析/test.py
+++ 数据分析/test.py
@@ -4,8 +4,15 @@ Created on Sat Dec 14 20:07:23 2019
 
 @author: Leq
 """
-from pandas import Series
-#
-s = Series(['a', True, 1], index=['first', 'secont', 'third'])
+# from pandas import Series
+# #
+# s = Series(['a', True, 1], index=['first', 'secont', 'third'])
 
-print(s[1])
+# print(s[1])
+
+
+import numpy
+numpy.arange(0.1, 0.5, 0.01)
+r = numpy.arange(0.1, 0.5, 0.01)
+
+print(r)
